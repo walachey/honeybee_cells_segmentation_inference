4f9292315209f0e890b452dd07c35ead9040c730
David Dormagen <czapper@gmx.de>
2023-05-02 17:28:29 +0200

    fixed wrong name for requirements file in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename):
     return [line for line in lines if line and not line.startswith("#")]
 
 
-reqs = parse_requirements("requirements_small_compat.txt")
+reqs = parse_requirements("requirements.txt")
 dep_links = [url for url in reqs if "http" in url]
 reqs = [req for req in reqs if "http" not in req]
 reqs += [url.split("egg=")[-1] for url in dep_links if "egg=" in url]
